c0dd7e10ce9aaf4827fc86c9bdf9f23061a3964b
Mars <marsbhoward@gmail.com>
2020-03-24 14:00:53 -0400

    add scroll to top on movie select

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -5,12 +5,24 @@ let title
 
 class Movie extends Component {
 
+componentDidMount() {
+  window.addEventListener("scroll", this.handeleScroll); // remove brackets ()
+}
+
+componentWillUnmount() {
+    window.removeEventListener("scroll", this.handeleScroll);
+}
+
+
+
 handleOnClick = () => {
     this.setState({
     currentMovie: this.props.movie,
     clicked: true
     },function () {
-      this.props.handler(this.state.currentMovie,this.state.clicked)
+    this.props.handler(this.state.currentMovie,this.state.clicked);
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
     })    
     
   }
@@ -23,7 +35,7 @@ handleOnClick = () => {
     title = this.props.movie.Title 
     if (title){   
       return (
-        <img onClick={this.handleOnClick} className= "movie" id={this.props.movie.Title} alt={this.props.movie.Title} src={this.props.movie.Poster}>
+        <img onClick={this.handleOnClick}  className= "movie" id={this.props.movie.Title} alt={this.props.movie.Title} src={this.props.movie.Poster}>
         </img>
       
       );
