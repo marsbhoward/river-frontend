dc5231a7837e96a8d51be37e6618cd74022709d7
Mars <marsbhoward@gmail.com>
2020-05-12 20:43:04 -0400

    create UserStreamsPage

---
+++ src/containers/UserStreamsPage.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchUserStreams } from '../actions/userStreamActions'
+import StreamList from '../components/StreamList'
+
+class UserStreamsPage extends Component {   
+  
+  componentDidMount() {
+    this.props.fetchUserStreams(this.props.userId)
+    console.log(this)
+  }
+//binds passed handler to StreamsPage handler
+  constructor(props){
+    super(props)
+    this.handler = this.handler.bind(this)
+  }
+// recieves id from passed handler
+  handler = (id,name) => {
+    this.setState({
+      currentStream: id,
+      currentStreamName: name
+    })
+ //returns the selected Stream id to App       
+    this.props.handler(id,name)
+  }
+  
+  handleLoading = () => {
+    if(this.props.loading) {
+      return <div>Loading Streams...</div>
+    } else {
+      
+      return <StreamList streamInfo={this.props.streamInfo} currentStream={this.props.currentStream} handler={this.handler}/>
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.handleLoading()}
+      </div>
+    );
+  }
+}
+
+
+const mapDispatchToProps = state => {
+  return {
+    userStreams: state.StreamsReducer.streams,
+    loading: state.StreamsReducer.loading,
+    currentStream: state.StreamsReducer.currentStream
+  }
+}
+
+export default connect(mapDispatchToProps, { fetchUserStreams })(UserStreamsPage)
